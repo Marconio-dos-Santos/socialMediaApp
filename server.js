d4e6dbb0bcb1e78ff855ee13c6fc0ac124729449
Marconio dos Santos <marconio.dev@gmail.com>
2022-10-01 22:17:42 -0300

    route to heroku

--- server.js
+++ server.js
@@ -15,13 +15,12 @@ const configRoutes = require("./routes/config");
 
 const path = require("path");
 
-
 // declare react files in build as static
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "views")));
 
 // serve index.html from the build folder
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "views", "index.ejs"));
 });
 
 
