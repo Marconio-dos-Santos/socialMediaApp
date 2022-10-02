07aa3e21fe8282593ab63c793dd1df42930f66ff
Marconio dos Santos <marconio.dev@gmail.com>
2022-10-01 21:40:48 -0300

    route to heroku

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.use(flash());
 
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
-app.use("/post", postRoutes);
+app.use("https://sociall-media-app.herokuapp.com/post", postRoutes);
 app.use("/comment", commentRoutes);
 app.use("/config", configRoutes)
 
